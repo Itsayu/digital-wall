6f8e02b07da72472b846926cb6414b391363b48a
Ayush Kumar <94097145+Itsayu@users.noreply.github.com>
2023-06-18 14:34:26 +0530

    Updated

--- src/component2/navbar2.js
+++ src/component2/navbar2.js
@@ -6,14 +6,14 @@ import { ArrowBack, Search, Bookmark} from '@mui/icons-material';
 const NavBar = () => {
     return (
         <nav>
-            <div className="navbar-container">
-                <div className="left">
+            <div className="navbar-container1">
+                <div className="left1">
                     <ArrowBack className="back-icon" />
                     <img src={logoImage} alt="Logo" className="logo" />
                     <h1 className="title">title</h1>
                 </div>
-                <div className="right">
-                    <Search className="search-icon" />
+                <div className="right1">
+                    <Search className="search-icon1" />
                     <div className="line"></div>
                     <Bookmark className="bookmark-icon" />
                 </div>
