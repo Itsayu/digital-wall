d9eb2ce300b0a8882c408de55e42969b5918728f
Ayush Kumar <94097145+Itsayu@users.noreply.github.com>
2023-06-18 20:18:04 +0530

    update

--- src/component2/PostList.js
+++ src/component2/PostList.js
@@ -1,131 +1,3 @@
-// import React, { useState } from 'react';
-// import Card from './card2';
-// import './PostList.css';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faPlus } from '@fortawesome/free-solid-svg-icons';
-
-// const PostList = () => {
-//   const [posts, setPosts] = useState([]);
-//   const [showPopup, setShowPopup]=useState(false);
-//   const [newPostData, setNewPostData]=useState({
-//     name: '',
-//     imageSrc: '',
-//     description: '',
-//    });
-
-//     const handleCreatePost = () => {
-//      setShowPopup(true);
-//     };
-
-//     const handlePopupClose = () => {
-//         setShowPopup(false);
-//     };
-
-//     const handleClose = () => {
-//       onClose();
-//     };
-
-//     const handleInputChange = (e) => {
-//         const { name, value } = e.target;
-//         setNewPostData({...newPostData, [name]: value});
-//     };
-
-//     const handleImageUpload =(e) => {
-//         const file = e.target.files[0];
-//         const reader = new FileReader();
-//         reader.onload=(event)=>{
-//             setNewPostData({...newPostData, imageSrc: event.target.result});
-//         };
-//         reader.readAsDataURL(file);
-//     };
-
-//     const handleCreateNewPost = () => {
-//         const { name, imageSrc, description } = newPostData;
-
-//         if(name && imageSrc && description) {
-//             const newPost = {
-//                 name,
-//                 imageSrc,
-//                 content: description,
-//             };
-//             setPosts([...posts, newPost]);
-//             setShowPopup(false);
-//             setNewPostData({
-//                 name: '',
-//                 imageSrc: '',
-//                 description: '',
-//             });
-//         }
-//     };
-
-//   const handleDeletePost = (index) => {
-//     const updatedPosts = [...posts];
-//     updatedPosts.splice(index, 1);
-//     setPosts(updatedPosts);
-//   };
-
-//   const handleEditPost = (index) => {
-//     console.log("Editing is happening");
-//   };
-
-//   return (
-//     <div>
-//       <div className='sndp'>
-//         <div>
-//             <h2>Your posts</h2>
-//         </div>
-//         <button className='create-post-btn' onClick={handleCreatePost}>
-//         <FontAwesomeIcon icon={faPlus}  className="plus-icon"/>
-//         Create new post
-//          </button>
-//     </div>
-//     {showPopup && (
-//         <div className="popup">
-//             <div className="popup-content">
-//                 <h2>Create New Post</h2>
-//                 <button className="close-button" onClick={handleClose}>
-//           <FontAwesomeIcon icon={faTimes} />
-//         </button>
-//                 <input
-//                     type="text"
-//                     name="name"
-//                     placeholder="Name"
-//                     value={newPostData.name}
-//                     onChange={handleInputChange}
-//                 />
-//                 <input
-//                     type="file"
-//                     name="imageSrc"
-//                     onChange={handleImageUpload}
-//                 />
-//                 <textarea
-//                     name="description"
-//                     placeholder="Description"
-//                     value={newPostData.description}
-//                     onChange={handleInputChange}
-//                 ></textarea>
-//                 <button onClick={handleCreateNewPost}>Create</button>
-//                 <button onClick={handlePopupClose}>Cancel</button>
-//             </div>
-
-//         </div>
-//         )}
-
-//       {posts.map((post, index) => (
-//         <Card
-//           key={index}
-//           name={post.name}
-//           imageSrc={post.imageSrc}
-//           content={post.content}
-//           onDelete={() => handleDeletePost(index)}
-//           onEdit={() => handleEditPost(index)}
-//         />
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default PostList;
 
 import React, { useState } from "react";
 import Card from "./card2";
@@ -214,9 +86,7 @@ const PostList = () => {
               </button>
             </div>
             <text className="text2">Write something for your post</text>
-            <div className="textHeading">
-              Subject
-            </div>
+            <div className="textHeading">Subject</div>
             <input
               type="text"
               name="name"
@@ -224,11 +94,9 @@ const PostList = () => {
               value={newPostData.name}
               onChange={handleInputChange}
             />
-            
+
             <input type="file" name="imageSrc" onChange={handleImageUpload} />
-            <div className="textHeading">
-              What's on your mind?
-            </div>
+            <div className="textHeading">What's on your mind?</div>
             <textarea
               name="description"
               placeholder="Description"
@@ -236,25 +104,27 @@ const PostList = () => {
               onChange={handleInputChange}
             ></textarea>
             <div className="submitButton">
-            <button onClick={handleCreateNewPost}>Publish</button>
-            {/* <button onClick={handlePopupClose}>Cancel</button> */}
+              <button onClick={handleCreateNewPost}>Publish</button>
+              {/* <button onClick={handlePopupClose}>Cancel</button> */}
             </div>
           </div>
         </div>
       )}
-
-      {posts.map((post, index) => (
-        <Card
-          key={index}
-          name={post.name}
-          imageSrc={post.imageSrc}
-          content={post.content}
-          onDelete={() => handleDeletePost(index)}
-          onEdit={() => handleEditPost(index)}
-        />
-      ))}
+      <div className="postListSection">
+        {posts.map((post, index) => (
+          <Card
+            key={index}
+            name={post.name}
+            imageSrc={post.imageSrc}
+            content={post.content}
+            onDelete={() => handleDeletePost(index)}
+            onEdit={() => handleEditPost(index)}
+          />
+        ))}
+      </div>
     </div>
   );
 };
 
 export default PostList;
+
--- src/component2/card2.js
+++ src/component2/card2.js
@@ -1,81 +1,482 @@
-import React, { useState } from 'react';
-import './card2.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart, faEllipsisV } from '@fortawesome/free-solid-svg-icons';
-import { Bookmark } from '@mui/icons-material';
+// import React, { useState } from "react";
+// import "./card2.css";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faHeart, faEllipsisV } from "@fortawesome/free-solid-svg-icons";
+// import { Bookmark } from "@mui/icons-material";
 
+// const Card = ({ name, imageSrc, content, onDelete, onEdit, onLike }) => {
+//   const [showMenu, setShowMenu] = useState(false);
+//   const [likeCount, setLikeCount] = useState(0);
+//   const [liked, setLiked] = useState(false);
+//   const handleLike = () => {
+//     if (liked) {
+//       setLikeCount(likeCount - 1);
+//       setLiked(false);
+//     } else {
+//       setLikeCount(likeCount + 1);
+//       setLiked(true);
+//     }
+//   };
+
+//   const handleMenuToggle = () => {
+//     setShowMenu(!showMenu);
+//   };
+
+//   const handleEdit = () => {
+//     onEdit();
+//     setShowMenu(false);
+//   };
+
+//   const handleDelete = () => {
+//     onDelete();
+//     setShowMenu(false);
+//   };
+
+//   const currentDate = new Date();
+//   const formattedDate = currentDate.toLocaleDateString();
+
+//   return (
+//       <div className="card2">
+//         <div className="card-header2">
+//           <h2 className="card-name2">{name}</h2>
+//           <div className="card-options2">
+//             <Bookmark className="bookmark-icon" />
+//             <button className="menu-button" onClick={handleMenuToggle}>
+//               <FontAwesomeIcon icon={faEllipsisV} />
+//             </button>
+//             {showMenu && (
+//               <ul className="menu-list">
+//                 <li onClick={handleEdit}>Edit</li>
+//                 <li onClick={handleDelete}>Delete</li>
+//               </ul>
+//             )}
+//           </div>
+//         </div>
+//         <div className="head-date">{formattedDate}</div>
+//         <img src={imageSrc} alt={name} className="card-image2" />
+//         <div className="card-content2">
+//           <p className="card-text2">{content}</p>
+//         </div>
+//         <div className="horizontal-line"></div>
+//         <div className="like">
+//           <div className="card-buttons2">
+//             <button className="like-button" onClick={handleLike}>
+//               <FontAwesomeIcon
+//                 icon={faHeart}
+//                 className={liked ? "heart-icon active" : "heart-icon"}
+//               />
+//             </button>
+//             <span className="like-count">{likeCount}</span>
+//           </div>
+//         </div>
+//       </div>
+//   );
+// };
+
+// export default Card;
+
+
+// import React, { useState } from "react";
+// import "./card2.css";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faHeart, faEllipsisV } from "@fortawesome/free-solid-svg-icons";
+// import { Bookmark } from "@mui/icons-material";
+
+// const Card = ({ name, imageSrc, content, onDelete, onEdit, onLike }) => {
+//   const [showMenu, setShowMenu] = useState(false);
+//   const [likeCount, setLikeCount] = useState(0);
+//   const [liked, setLiked] = useState(false);
+//   const [bookmarked, setBookmarked] = useState(false);
+
+//   const handleLike = () => {
+//     if (liked) {
+//       setLikeCount(likeCount - 1);
+//       setLiked(false);
+//     } else {
+//       setLikeCount(likeCount + 1);
+//       setLiked(true);
+//     }
+//   };
+
+//   const handleBookmark = () => {
+//     setBookmarked(!bookmarked);
+//   };
+
+//   const handleMenuToggle = () => {
+//     setShowMenu(!showMenu);
+//   };
+
+//   const handleEdit = () => {
+//     onEdit();
+//     setShowMenu(false);
+//   };
+
+//   const handleDelete = () => {
+//     onDelete();
+//     setShowMenu(false);
+//   };
+
+//   const currentDate = new Date();
+//   const formattedDate = currentDate.toLocaleDateString();
+
+//   return (
+//     <div className="card2">
+//       <div className="card-header2">
+//         <h2 className="card-name2">{name}</h2>
+//         <div className="card-options2">
+//           <Bookmark
+//             className={`bookmark-icon ${bookmarked ? "active" : ""}`}
+//             onClick={handleBookmark}
+//           />
+//           <button className="menu-button" onClick={handleMenuToggle}>
+//             <FontAwesomeIcon icon={faEllipsisV} />
+//           </button>
+//           {showMenu && (
+//             <ul className="menu-list">
+//               <li onClick={handleEdit}>Edit</li>
+//               <li onClick={handleDelete}>Delete</li>
+//             </ul>
+//           )}
+//         </div>
+//       </div>
+//       <div className="head-date">{formattedDate}</div>
+//       <img src={imageSrc} alt={name} className="card-image2" />
+//       <div className="card-content2">
+//         <p className="card-text2">{content}</p>
+//       </div>
+//       <div className="horizontal-line"></div>
+//       <div className="like">
+//         <div className="card-buttons2">
+//           <button className="like-button" onClick={handleLike}>
+//             <FontAwesomeIcon
+//               icon={faHeart}
+//               className={liked ? "heart-icon active" : "heart-icon"}
+//             />
+//           </button>
+//           <span className="like-count">{likeCount}</span>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Card;
+
+
+// import React, { useState } from "react";
+// import "./card2.css";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faHeart, faEllipsisV, faCheck, faTimes } from "@fortawesome/free-solid-svg-icons";
+// import { Bookmark } from "@mui/icons-material";
+
+// const Card = ({ name, imageSrc, content, onDelete, onEdit, onLike }) => {
+//   const [showMenu, setShowMenu] = useState(false);
+//   const [likeCount, setLikeCount] = useState(0);
+//   const [liked, setLiked] = useState(false);
+//   const [bookmarked, setBookmarked] = useState(false);
+//   const [editing, setEditing] = useState(false);
+//   const [editedContent, setEditedContent] = useState(content);
+
+//   const handleLike = () => {
+//     if (liked) {
+//       setLikeCount(likeCount - 1);
+//       setLiked(false);
+//     } else {
+//       setLikeCount(likeCount + 1);
+//       setLiked(true);
+//     }
+//   };
+
+//   const handleBookmark = () => {
+//     setBookmarked(!bookmarked);
+//   };
+
+//   const handleMenuToggle = () => {
+//     setShowMenu(!showMenu);
+//   };
+
+//   const handleEdit = () => {
+//     if (!editing) {
+//       setEditing(true);
+//     } else {
+//       onEdit(editedContent);
+//       setEditing(false);
+//     }
+//     setShowMenu(false);
+//   };
+
+//   const handleDelete = () => {
+//     onDelete();
+//     setShowMenu(false);
+//   };
+
+//   const handleInputChange = (e) => {
+//     setEditedContent(e.target.value);
+//   };
+
+//   const currentDate = new Date();
+//   const formattedDate = currentDate.toLocaleDateString();
+
+//   return (
+//     <div className="card2">
+//       <div className="card-header2">
+//         <h2 className="card-name2">{name}</h2>
+//         <div className="card-options2">
+//           <Bookmark
+//             className={`bookmark-icon ${bookmarked ? "active" : ""}`}
+//             onClick={handleBookmark}
+//           />
+//           <button className="menu-button" onClick={handleMenuToggle}>
+//             <FontAwesomeIcon icon={faEllipsisV} />
+//           </button>
+//           {showMenu && (
+//             <ul className="menu-list">
+//               <li onClick={handleEdit}>{editing ? "Save" : "Edit"}</li>
+//               <li onClick={handleDelete}>Delete</li>
+//             </ul>
+//           )}
+//         </div>
+//       </div>
+//       <div className="head-date">{formattedDate}</div>
+//       <img src={imageSrc} alt={name} className="card-image2" />
+//       <div className="card-content2">
+//         {editing ? (
+//           <textarea
+//             className="edit-textarea"
+//             value={editedContent}
+//             onChange={handleInputChange}
+//           />
+//         ) : (
+//           <p className="card-text2">{content}</p>
+//         )}
+//       </div>
+//       <div className="horizontal-line"></div>
+//       <div className="like">
+//         <div className="card-buttons2">
+//           <button className="like-button" onClick={handleLike}>
+//             <FontAwesomeIcon
+//               icon={faHeart}
+//               className={liked ? "heart-icon active" : "heart-icon"}
+//             />
+//           </button>
+//           <span className="like-count">{likeCount}</span>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Card;
+
+// import React, { useState } from "react";
+// import "./card2.css";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faHeart, faEllipsisV, faCheck, faTimes } from "@fortawesome/free-solid-svg-icons";
+// import { Bookmark } from "@mui/icons-material";
+
+// const Card = ({ name, imageSrc, content, onDelete, onEdit, onLike }) => {
+//   const [showMenu, setShowMenu] = useState(false);
+//   const [likeCount, setLikeCount] = useState(0);
+//   const [liked, setLiked] = useState(false);
+//   const [bookmarked, setBookmarked] = useState(false);
+//   const [editing, setEditing] = useState(false);
+//   const [editedContent, setEditedContent] = useState(content);
+
+//   const handleLike = () => {
+//     if (liked) {
+//       setLikeCount(likeCount - 1);
+//       setLiked(false);
+//     } else {
+//       setLikeCount(likeCount + 1);
+//       setLiked(true);
+//     }
+//   };
+
+//   const handleBookmark = () => {
+//     setBookmarked(!bookmarked);
+//   };
+
+//   const handleMenuToggle = () => {
+//     setShowMenu(!showMenu);
+//   };
+
+//   const handleEdit = () => {
+//     if (!editing) {
+//       setEditing(true);
+//     } else {
+//       onEdit(editedContent);
+//       setEditing(false);
+//       setEditedContent(content); // Reset editedContent to the original content
+//     }
+//     setShowMenu(false);
+//   };
+
+//   const handleDelete = () => {
+//     onDelete();
+//     setShowMenu(false);
+//   };
+
+//   const handleInputChange = (e) => {
+//     setEditedContent(e.target.value);
+//   };
+
+//   const currentDate = new Date();
+//   const formattedDate = currentDate.toLocaleDateString();
+
+//   return (
+//     <div className="card2">
+//       <div className="card-header2">
+//         <h2 className="card-name2">{name}</h2>
+//         <div className="card-options2">
+//           <Bookmark
+//             className={`bookmark-icon ${bookmarked ? "active" : ""}`}
+//             onClick={handleBookmark}
+//           />
+//           <button className="menu-button" onClick={handleMenuToggle}>
+//             <FontAwesomeIcon icon={faEllipsisV} />
+//           </button>
+//           {showMenu && (
+//             <ul className="menu-list">
+//               <li onClick={handleEdit}>{editing ? "Save" : "Edit"}</li>
+//               <li onClick={handleDelete}>Delete</li>
+//             </ul>
+//           )}
+//         </div>
+//       </div>
+//       <div className="head-date">{formattedDate}</div>
+//       <img src={imageSrc} alt={name} className="card-image2" />
+//       <div className="card-content2">
+//         {editing ? (
+//           <textarea
+//             className="edit-textarea"
+//             value={editedContent}
+//             onChange={handleInputChange}
+//           />
+//         ) : (
+//           <p className="card-text2">{content}</p>
+//         )}
+//       </div>
+//       <div className="horizontal-line"></div>
+//       <div className="like">
+//         <div className="card-buttons2">
+//           <button className="like-button" onClick={handleLike}>
+//             <FontAwesomeIcon
+//               icon={faHeart}
+//               className={liked ? "heart-icon active" : "heart-icon"}
+//             />
+//           </button>
+//           <span className="like-count">{likeCount}</span>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Card;
+
+
+
+import React, { useState } from "react";
+import "./card2.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart, faEllipsisV, faCheck, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { Bookmark } from "@mui/icons-material";
 
 const Card = ({ name, imageSrc, content, onDelete, onEdit, onLike }) => {
-    const [showMenu, setShowMenu] = useState(false);
-    const [likeCount, setLikeCount] = useState(0);
-    const [liked, setLiked] = useState(false);
-    const handleLike = () => {
-        if (liked) {
-            setLikeCount(likeCount - 1);
-            setLiked(false);
-        } else {
-            setLikeCount(likeCount + 1);
-            setLiked(true);
-        }
-    };
+  const [showMenu, setShowMenu] = useState(false);
+  const [likeCount, setLikeCount] = useState(0);
+  const [liked, setLiked] = useState(false);
+  const [bookmarked, setBookmarked] = useState(false);
+  const [editing, setEditing] = useState(false);
+  const [editedContent, setEditedContent] = useState(content);
 
+  const handleLike = () => {
+    if (liked) {
+      setLikeCount(likeCount - 1);
+      setLiked(false);
+    } else {
+      setLikeCount(likeCount + 1);
+      setLiked(true);
+    }
+  };
 
-    const handleMenuToggle = () => {
-        setShowMenu(!showMenu);
-    };
+  const handleBookmark = () => {
+    setBookmarked(!bookmarked);
+  };
 
-    const handleEdit = () => {
-        onEdit();
-        setShowMenu(false);
-    };
+  const handleMenuToggle = () => {
+    setShowMenu(!showMenu);
+  };
 
-    const handleDelete = () => {
-        onDelete();
-        setShowMenu(false);
-    };
+  const handleEdit = () => {
+    if (!editing) {
+      setEditing(true);
+    } else {
+      onEdit(editedContent);
+      setEditing(false);
+    }
+    setShowMenu(false);
+  };
 
-    const currentDate = new Date();
-    const formattedDate = currentDate.toLocaleDateString();
+  const handleDelete = () => {
+    onDelete();
+    setShowMenu(false);
+  };
 
-    return (
-        
-        <div className="card2">
-            <div className="card-header2">
-                <h2 className="card-name2">{name}</h2>
-                <div className="card-options2">
-                     <Bookmark className="bookmark-icon" />
-                     <button className="menu-button" onClick={handleMenuToggle}>
-                        <FontAwesomeIcon icon={faEllipsisV} />
-                    </button>
-                    {showMenu && (
-                        <ul className="menu-list">
-                            <li onClick={handleEdit}>Edit</li>
-                            <li onClick={handleDelete}>Delete</li>
-                        </ul>
-                    )}
-                </div>
-            </div>
-            <div className='head-date'>{formattedDate}</div>
-            <img src={imageSrc} alt={name} className="card-image2" />
-            <div className="card-content2">
-                <p className="card-text2">{content}</p>
-            </div>
-            <div className="horizontal-line"></div>
-            <div className="like">
-                <div className="card-buttons2">
-                    <button className="like-button" onClick={handleLike}>
-                        <FontAwesomeIcon icon={faHeart} className={liked ? 'heart-icon active' : 'heart-icon'} />
-                    </button>
-                    <span className="like-count">{likeCount}</span>
-                </div>
-            </div>
+  const handleInputChange = (e) => {
+    setEditedContent(e.target.value);
+  };
 
+  const currentDate = new Date();
+  const formattedDate = currentDate.toLocaleDateString();
 
+  return (
+    <div className="card2">
+      <div className="card-header2">
+        <h2 className="card-name2">{name}</h2>
+        <div className="card-options2">
+          <Bookmark
+            className={`bookmark-icon ${bookmarked ? "active" : ""}`}
+            onClick={handleBookmark}
+          />
+          <button className="menu-button" onClick={handleMenuToggle}>
+            <FontAwesomeIcon icon={faEllipsisV} />
+          </button>
+          {showMenu && (
+            <ul className="menu-list">
+              <li onClick={handleEdit}>{editing ? "Save" : "Edit"}</li>
+              <li onClick={handleDelete}>Delete</li>
+            </ul>
+          )}
         </div>
-
-    );
+      </div>
+      <div className="head-date">{formattedDate}</div>
+      <img src={imageSrc} alt={name} className="card-image2" />
+      <div className="card-content2">
+        {editing ? (
+          <textarea
+            className="edit-textarea"
+            value={editedContent}
+            onChange={handleInputChange}
+          />
+        ) : (
+          <p className="card-text2">{content}</p>
+        )}
+      </div>
+      <div className="horizontal-line"></div>
+      <div className="like">
+        <div className="card-buttons2">
+          <button className="like-button" onClick={handleLike}>
+            <FontAwesomeIcon
+              icon={faHeart}
+              className={liked ? "heart-icon active" : "heart-icon"}
+            />
+          </button>
+          <span className="like-count">{likeCount}</span>
+        </div>
+      </div>
+    </div>
+  );
 };
-
-
 
 export default Card;
